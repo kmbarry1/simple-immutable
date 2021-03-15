3f335daa7b1bcf5526f9a568849df5eed89bdd13
Kurt Barry <kurt.m.barry@gmail.com>
2021-03-15 13:13:07 -0400

    add dummy contract with immutable var

---
+++ dapp/src/SimpleImmutable.sol
@@ -0,0 +1,12 @@
+pragma solidity =0.6.12;
+
+contract SimpleImmutable {
+    uint256 public immutable foo;
+    constructor(uint256 value) public {
+        foo = value;
+    }
+    function timesFoo(uint256 num) external view returns (uint256 retVal) {
+        retVal = foo * num;
+        require(num == 0 || retVal / num == foo, "overflow");
+    }
+}
